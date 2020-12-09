2dca1512360e3766bf110ca3bc0e444bbbee4975
Shai Rubinstein <rubinsh@gmail.com>
2020-12-09 13:15:39 +0200

    updated working dir

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,7 @@ Xvfb -ac :99 -screen 0 1280x1024x16 > /dev/null 2>&1 &
 export DISPLAY=:99.0
 export PUPPETEER_EXEC_PATH="google-chrome-stable"
 
+cd integration-tests
 # Run commands
 cmd=$@
 echo "Running '$cmd'!"
